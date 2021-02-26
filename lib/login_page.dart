99ce04d0ce06cd7276adc1637b54543fa330e777
Fahrul Azimi <fahrul.azimi.2000@gmail.com>
2021-02-26 16:55:16 +0700

    update login baru

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,133 +7,169 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  bool _passwordVisible = true;
+bool _passwordVisible = false;
 
-  void initState() {
-    _passwordVisible = false;
-  }
+void initState() {
+  _passwordVisible = true;
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+
+  String _username, _password;
+  bool _btnEnabled = false;
 
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        padding: EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  "Selamat Datang",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "Silahkan login terlebih dahulu",
-                  style: TextStyle(fontSize: 14),
-                ),
-                Image.asset(
-                  "assets/images/intro4.png",
-                  width: SizeConfig.blockHorizontal * 100,
-                  height: SizeConfig.blockVertical * 30,
-                ),
-                Text(
-                  "Username",
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
-                ),
-                Container(
-                  padding: EdgeInsets.only(left: 10),
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey[100]),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    height: 50,
                   ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Masukkan Username",
-                        hintStyle:
-                            TextStyle(fontSize: 14, color: Colors.grey[400])),
+                  Text(
+                    "Selamat Datang",
+                    style: TextStyle(
+                        fontFamily: "NuninoSans",
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700),
                   ),
-                ),
-                Text(
-                  "Password",
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
-                ),
-                Container(
-                  padding: EdgeInsets.only(left: 10),
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey[100]),
+                  SizedBox(
+                    height: 10,
                   ),
-                  child: TextField(
-                      obscureText: !_passwordVisible,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Masukkan Password",
-                        hintStyle:
-                            TextStyle(fontSize: 14, color: Colors.grey[400]),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                              _passwordVisible
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
-                              color: Colors.red),
-                          onPressed: () {
-                            setState(() {
-                              _passwordVisible = !_passwordVisible;
-                            });
-                          },
-                          color: Colors.red,
-                        ),
+                  Text(
+                    "Silahkan login terlebih dahulu",
+                    style: TextStyle(fontFamily: "NuninoSans", fontSize: 14),
+                  ),
+                  Image.asset(
+                    "assets/images/intro4.png",
+                    width: SizeConfig.blockHorizontal * 100,
+                    height: SizeConfig.blockVertical * 30,
+                  ),
+                  Form(
+                      key: formKey,
+                      onChanged: () => setState(
+                          () => _btnEnabled = formKey.currentState.validate()),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Username",
+                            style: TextStyle(
+                                fontFamily: "NuninoSans",
+                                fontSize: 14,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(left: 10),
+                            margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey[100]),
+                            ),
+                            child: TextFormField(
+                              onSaved: (input) => _username = input,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Masukkan Username",
+                                  hintStyle: TextStyle(
+                                      fontFamily: "NuninoSans",
+                                      fontSize: 14,
+                                      color: Colors.grey[400])),
+                            ),
+                          ),
+                          Text(
+                            "Password",
+                            style: TextStyle(
+                                fontFamily: "NuninoSans",
+                                fontSize: 14,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(left: 10),
+                            margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey[100]),
+                            ),
+                            child: TextFormField(
+                                onSaved: (input) => _password = input,
+                                obscureText: !_passwordVisible,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Masukkan Password",
+                                  hintStyle: TextStyle(
+                                      fontFamily: "NuninoSans",
+                                      fontSize: 14,
+                                      color: Colors.grey[400]),
+                                  suffixIcon: IconButton(
+                                    icon: Icon(
+                                        _passwordVisible
+                                            ? Icons.visibility
+                                            : Icons.visibility_off,
+                                        color: Colors.red),
+                                    onPressed: () {
+                                      setState(() {
+                                        _passwordVisible = !_passwordVisible;
+                                      });
+                                    },
+                                    color: Colors.red,
+                                  ),
+                                )),
+                          ),
+                        ],
                       )),
-                ),
-                Container(
-                  child: Center(
-                    child: Text(
-                      "Butuh bantuan?",
-                      style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.red),
+                  Container(
+                    child: Center(
+                      child: Text(
+                        "Butuh bantuan?",
+                        style: TextStyle(
+                            fontFamily: "NuninoSans",
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.red),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 35,
-                ),
-                GestureDetector(
-                    child: Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25),
-                        gradient: LinearGradient(
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                            colors: [Colors.grey, Colors.grey[200]]),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "LOGIN",
-                          style: TextStyle(fontSize: 14, color: Colors.white),
+                  SizedBox(
+                    height: 35,
+                  ),
+                  GestureDetector(
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                              colors: _btnEnabled
+                                  ? [Colors.red, Colors.red]
+                                  : [Colors.grey, Colors.grey[200]]),
                         ),
+                        child: Center(
+                            child: Text(
+                          "LOGIN",
+                          style: TextStyle(
+                              fontFamily: "NuninoSans",
+                              fontSize: 14,
+                              color: Colors.white),
+                        )),
                       ),
-                    ),
-                    onTap: () {
-                      Navigator.pushReplacement(context,
-                          MaterialPageRoute(builder: (context) => Home()));
-                    })
-              ],
-            ),
-          ],
+                      onTap: () {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) => Home()));
+                      })
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
